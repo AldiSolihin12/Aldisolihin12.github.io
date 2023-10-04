1e7cfc91a9606271b9e8393218d96163321ee40b
AldiSolihin12 <110613271+AldiSolihin12@users.noreply.github.com>
2023-10-04 23:19:41 +0800

    Add files via upload

---
+++ Posttest3/javascript.js
@@ -0,0 +1,58 @@
+const menuToggle = document.getElementById('menu-toggle');
+const buka = document.getElementById('menuBuka');
+const tutup = document.getElementById('menuTutup');
+const menu = document.getElementById('menu');
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    if (buka.style.display == 'none'){
+        buka.style.display = 'block';
+        tutup.style.display = 'none';
+    } else {
+        buka.style.display = 'none';
+        tutup.style.display = 'block';
+    }
+});
+
+function mode(){
+    var light = document.getElementById("light")
+    var night = document.getElementById("night")
+    light.classList.toggle("dark");
+    night.classList.toggle("dark");
+
+    const ubah1 = document.body;
+    const ubah2 = document.querySelector('.navbar');
+    var ubah3 = document.getElementById('logo2');
+    var ubah4 = document.getElementById('logo3');
+    const ubah5 = document.getElementById("webicon");
+    
+    menu.classList.toggle("dark1")
+    menuToggle.classList.toggle("dark")
+    ubah1.classList.toggle("dark1");
+    ubah2.classList.toggle("dark1");
+    ubah3.classList.toggle("dark");
+    ubah4.classList.toggle("dark");
+    ubah5.classList.toggle("dark");
+
+    if (light.style.display == 'none'){
+        light.style.display = 'block';
+        night.style.display = 'none';
+    } else {
+        light.style.display = 'none';
+        night.style.display = 'block';
+    }
+}
+
+var link = document.getElementById('download');
+link.addEventListener('click', function (event) {
+    var konfirmasi = confirm('Apakah Anda yakin ingin melanjutkan?');
+    if (!konfirmasi) {
+        event.preventDefault(); 
+    }
+});
+
+var tombol = document.getElementById('tombol');
+var teks = document.getElementById('teks');
+tombol.addEventListener('click', function () {
+    teks.style.display = 'block';
+    tombol.style.display = 'none';
+});

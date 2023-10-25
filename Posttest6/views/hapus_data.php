abc7c7f8ca074c01450f25d02e5b0930f72e2511
AldiSolihin12 <110613271+AldiSolihin12@users.noreply.github.com>
2023-10-25 22:02:10 +0800

    Add files via upload

---
+++ Posttest6/views/hapus_data.php
@@ -0,0 +1,32 @@
+<?php
+    require "../include/connect.php";
+    $id = $_GET['id'];
+    $gambar_lama = mysqli_query($conn, "SELECT gambar FROM karakter_genshin where id = $id"); 
+    $row = mysqli_fetch_assoc($gambar_lama);
+    $nama_file = $row['gambar'];
+
+    $get = mysqli_query($conn, "DELETE FROM karakter_genshin WHERE id = $id");
+    $karakter = [];
+
+    $lokasi_file = '../assets/uploadedImg/' . $nama_file; 
+
+    if($nama_file != "NoImage.png"){
+        if (file_exists($lokasi_file)) {
+            unlink($lokasi_file);
+        } 
+    } 
+
+    if ($get) {
+        echo "
+        <script>
+            alert('Data berhasil dihapus!');
+            document.location.href = 'dashboard.php';
+        </script>";
+    } else {
+        echo "
+        <script>
+            alert('Data gagal dihapus!');
+            document.location.href = 'dashboard.php';
+        </script>";
+    }
+?>
